5a1422a17dae37478716761a99b1a2c61f812a01
Evne Lviv 02 <evnelviv02@EDL02.local>
2019-01-10 16:54:14 +0200

    #13094 Authorization for 3 roles. Implemented registration and authorization users

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index(){
         return view('backend.dashboard.index');
     }
